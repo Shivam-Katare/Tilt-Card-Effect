36091a0a0f78c64d7e2084a5f113ea5c0457024c
Shivam Katare <katare27451@gmail.com>
2022-07-18 17:30:03 +0530

    Format the document

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-// by
-// abubakersaeed.netlify.com | @AbubakerSaeed96
-// ============================================
-
-// Inspiration:
-// Tilt.js: https://gijsroge.github.io/tilt.js/
-// Andy Merskin's parallax depth cards pen: https://codepen.io/andymerskin/full/XNMWvQ/
-
-// Thank You for Viewing
-
 class parallaxTiltEffect {
 
   constructor({ element, tiltEffect }) {
